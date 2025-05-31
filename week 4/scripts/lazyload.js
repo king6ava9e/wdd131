84e3cb444105c8ca563cda48f0bf82f538614434
King6ava9e <acheampongkingsley2003@gmail.com>
2025-05-30 22:23:34 -0700

    assignment on filtered temple

---
+++ week 4/scripts/lazyload.js
@@ -0,0 +1,7 @@
+// Set current year in footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date in footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+const menuBtn = document.getElementById("menu-btn");
+const navMenu = document.getElementById("nav-menu");
